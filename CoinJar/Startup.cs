efaabbe925853bca1f39127bdd24d5836d433a18
VishavP <vishaav.messi.premlall@gmail.com>
2020-12-12 16:51:33 +0200

    fixed startup

--- CoinJar/Startup.cs
+++ CoinJar/Startup.cs
@@ -22,7 +22,6 @@ namespace CoinJar
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
@@ -36,25 +35,14 @@ namespace CoinJar
             });
             services.AddTransient<ICoinJar, Logic.Implementation.CoinJar>();
             services.AddTransient<ICoin, Coin>();
-            //services.AddSwaggerGen(c =>
-            //{
-            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "CoinJar API", Version = "v1" });
-            //    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-            //    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-            //    c.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
-            //});
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseDeveloperExceptionPage();
 
-            // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
 
-            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
-            // specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
